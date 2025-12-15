21a2c3ab6f1f8facf1bbeded2700f31dd1c71e7a
breedware limited <breedware@gmail.com>
2025-12-15 13:07:07 +0100

    auth  provider exported

--- src/client/index.ts
+++ src/client/index.ts
@@ -3,3 +3,4 @@ export { useAuth } from '../client/useAuth';
 export { useCount } from '../client/useCounter';
 export { useFetch } from '../client/useFetch';
 export { useStream } from '../client/useStream';
+export { AUTHProvider, AUTHContext } from '../client/AuthProvider.provider'
